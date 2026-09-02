93868803b2c12eee04821d9d26f76650927849f4
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-02 21:13:46 +0330

    create Electronic class: task3

---
+++ HomeWork3/task3/Electronic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3.task3
+{
+    public class Electronic : Product
+    {
+        public DateTime WarrantyPeriod { get; set; }
+
+        public Electronic(string name, decimal price, DateTime warrantyPeriod):base(name, price)
+        {
+            warrantyPeriod = WarrantyPeriod;
+        }
+        
+        public override string GetProductDetails()
+        {
+            return base.GetProductDetails() + $"WarrantyPeriod: {WarrantyPeriod}";
+        }
+
+    }
+}
