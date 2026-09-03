522ec459aa5624e200d3df93eb33962092ea9bc1
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-03 21:53:41 +0330

    create classes: doctor, patient, person, room,roomFullException(without patterns)

---
+++ HomeWork3/task4/without designPatterns/Patient.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace HomeWork3.task4.without_designPatterns
+{
+    public class Patient: Person
+    {
+        public string PatientId { get; set; }
+        public List<string> MedicalHistory { get; set; } = new List<string>();
+
+        public Patient() { }
+        public Patient(string name, int age, string nationalId, string patientId): base(name, age, nationalId) 
+        { 
+            PatientId = patientId;
+            
+        }
+        public void AddToMedicalHistory(string disease) 
+        {
+            MedicalHistory.Add(disease);
+        }
+    }
+}
---
+++ HomeWork3/task4/without designPatterns/Room.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace HomeWork3.task4.without_designPatterns
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public List<Patient> Patients { get; set; } = new List<Patient>();
+        public Room() { }
+        public Room(int roomNumber, int capacity) 
+        {
+            RoomNumber = roomNumber;
+            Capacity = capacity;
+        }
+        public bool HasAvailableSpace => Patients.Count < Capacity;
+        public void AssignPatient(Patient patient) 
+        {
+            if (Patients.Count >= Capacity) 
+            {
+                throw new RoomFullException($"Room {RoomNumber} is full");
+            }
+            Patients.Add(patient);
+        }
+    }
+}
